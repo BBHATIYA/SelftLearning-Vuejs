d1b07599750128c06c79fb8cfcf22c2cb20215c0
BBHATIYA <BHATIYABHAVESH03@GMAIL.COM>
2022-10-02 14:47:24 +0300

    computed prop & watchers

--- EventBinding/app.js
+++ EventBinding/app.js
@@ -1,20 +1,55 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 10,
+      counter: 0,
       name: "",
-      confirmedName: "",
+      lastName: "",
+      // fullname: "",
     };
   },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 2000);
+      }
+    },
+    // name(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     this.fullname = value + " " + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     this.fullname = this.name + " " + value;
+    //   }
+    // },
+  },
+  computed: {
+    fullname() {
+      console.log("running again...");
+      if (this.name === "" || this.lastName === "") {
+        return "";
+      }
+      return this.name + " " + this.lastName;
+    },
+  },
   methods: {
-    confirmedInput() {
-      this.confirmedName = this.name;
+    outputFullname() {
+      console.log("running again...");
+      if (this.name === "") {
+        return "";
+      }
+      return this.name + " " + "Bhatiya";
     },
-    submitForm() {
-      alert("Submitted!");
-    },
-    setName(event, lastName) {
-      this.name = event.target.value + " " + lastName;
+    setName(event) {
+      this.name = event.target.value;
     },
     add(num) {
       this.counter = this.counter + num;
@@ -22,6 +57,9 @@ const app = Vue.createApp({
     reduce(num) {
       this.counter = this.counter - num;
       // this.counter--;
+    },
+    resetInput() {
+      this.name = "";
     },
   },
 });
